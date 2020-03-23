48d682368abc241844848d4d170d4169e5fd0202
赵鑫 <zhaoxin01@ke.com>
2020-03-23 19:24:43 +0800

    modified

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,8 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');//自动创建html文件
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');//清除多余文件
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  /*
+  * 当 webpack 打包源代码时，可能会很难追踪到错误和警告在源代码中的原始位置。
+  * 例如，如果将三个源文件（a.js, b.js 和 c.js）打包到一个 bundle（bundle.js）中，
+  * 而其中一个源文件包含一个错误，那么堆栈跟踪就会简单地指向到 bundle.js。
+  * 这并通常没有太多帮助，因为你可能需要准确地知道错误来自于哪个源文件。
+  * source map 有很多不同的选项可用，这里使用 inline-source-map 选项
+  * 其他可查阅文档：https://www.webpackjs.com/configuration/devtool/
+  */
   devtool: 'inline-source-map',
   entry: {
     index: './src/index.js'
@@ -14,9 +23,6 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.css$/,
-      use: 'style-loader!css-loader'
-    }, {
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"], // 加载时顺序从右向左
     }, {
